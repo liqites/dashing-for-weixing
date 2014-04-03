e1e6abe779c7d62d9e7da2f6767b1c7e36b99593
tesla lee <leechee89@gmail.com>
2014-04-03 18:54:30 +0800

    data

---
+++ jobs/static.rb
@@ -0,0 +1,180 @@
+require_relative '../modules/generator'
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+#@@
+prand = Random.new(1234)
+#@@ 前30天的日期
+date_30b = Generator.generate_date(20)
+#@@ 前7天和后15天的日期
+date_7b15f = Generator.generate_date(22,Time.now+15*24*3600)
+#@@ 后30天日期
+date_30f = Generator.generate_date(20,Time.now+30*24*3600)
+###########
+# 金属闭口
+###########
+#@@前30天核定产能
+metalclose_aprovedplan = Generator.generate_data(20,45,30)
+#@@前30天计划数
+metalclose_plan_30b = Generator.generate_data(20,50.0,20.0)
+#@@前30天完工数
+metalclose_finished = Generator.generate_data(20,49.0,10.0)
+#@@前30天实际产量
+metalclose_trueoutput = Generator.generate_data(20,70.0,20.0)
+#@@前30天缺数 = 计划数 - 完工数
+metalclose_unfinished =Generator.generate_array_minus(metalclose_plan_30b,metalclose_finished)
+#@@后15天计划数
+metalclose_plan_30f = Generator.generate_data(20,50.0,20.0)
+#---------------------------
+#@@当日实际产量 //定时job
+#---------------------------
+
+# 前一个月缺数、完工数、核定产能对比
+send_event('metalclose_capicity', {date:date_30b, plan:metalclose_aprovedplan, finished:metalclose_finished, unfinished:metalclose_unfinished })
+# 今日实际产量与前一个月（31个点）实际产量对比
+send_event('metalclose_output',{date:date_30b,output:metalclose_trueoutput})
+# 前7天与后15天的计划组成
+send_event('metalclose_plan',{date:date_7b15f,plan:(metalclose_plan_30b.last(7)<<metalclose_plan_30f.first(15)),finished:Generator.generate_new_array(22,7,metalclose_finished),unfinished:Generator.generate_array_minus((metalclose_plan_30b.last(7)<<metalclose_plan_30f.first(15)),Generator.generate_new_array(22,7,metalclose_finished))})
+# 前一个月的累计误期值
+send_event('metalclose_sum_unfinished',{value:Generator.generate_array_sum(metalclose_unfinished)})
+# 前一个月的存量值+后15天的计划值
+send_event('metalclose_sum_planunfinished',{value:(Generator.generate_array_sum(metalclose_plan_30b)+Generator.generate_array_sum(metalclose_plan_30f.first(15)))})
+# 前一个月的实际产量周日均与实际产量的对比
+
+
+# 后30天的每日计划量线性图
+send_event('mentalclose_plan_30f',{date:date_30f,plan:metalclose_plan_30f})
+
+
+###########
+# 金属开口
+###########
+#@@前30天核定产能
+metalopen_aprovedplan = Generator.generate_data(20,35,20)
+#@@前30天计划数
+metalopen_plan_30b = Generator.generate_data(20,40.0,10.0)
+#@@前30天完工数
+metalopen_finished = Generator.generate_data(20,39.0,10.0)
+#@@前30天实际产量
+metalopen_trueoutput = Generator.generate_data(20,50.0,10.0)
+#@@前30天缺数 = 计划数 - 完工数
+metalopen_unfinished =Generator.generate_array_minus(metalopen_plan_30b,metalopen_finished)
+#@@后15天计划数
+metalopen_plan_30f = Generator.generate_data(20,40.0,10.0)
+#---------------------------
+#@@当日实际产量 //定时job
+#---------------------------
+
+# 前一个月缺数、完工数、核定产能对比
+send_event('metalopen_capicity', {date:date_30b, plan:metalopen_aprovedplan, finished:metalopen_finished, unfinished:metalopen_unfinished })
+# 今日实际产量与前一个月（31个点）实际产量对比
+send_event('metalopen_output',{date:date_30b,output:metalopen_trueoutput})
+# 前7天与后15天的计划组成
+send_event('metalopen_plan',{date:date_7b15f,plan:(metalopen_plan_30b.last(7)<<metalopen_plan_30f.first(15)),finished:Generator.generate_new_array(22,7,metalopen_finished),unfinished:Generator.generate_array_minus((metalopen_plan_30b.last(7)<<metalopen_plan_30f.first(15)),Generator.generate_new_array(22,7,metalopen_finished))})
+# 前一个月的累计误期值
+send_event('metalopen_sum_unfinished',{value:Generator.generate_array_sum(metalopen_unfinished)})
+# 前一个月的存量值+后15天的计划值
+send_event('metalopen_sum_planunfinished',{value:(Generator.generate_array_sum(metalopen_plan_30b)+Generator.generate_array_sum(metalopen_plan_30f.first(15)))})
+# 前一个月的实际产量周日均与实际产量的对比
+
+
+# 后30天的每日计划量线性图
+send_event('mentalopen_plan_30f',{date:date_30f,plan:metalopen_plan_30f})
+
+###########
+# 金属打样
+###########
+#@@前30天计划数
+metalproofing_plan_30b = Generator.generate_data(20,50.0,20.0)
+#@@前30天完工数
+metalproofing_finished = Generator.generate_data(20,49.0,10.0)
+#@@前30天实际产量
+metalproofing_trueoutput = Generator.generate_data(20,70.0,20.0)
+#@@前30天缺数 = 计划数 - 完工数
+metalproofing_unfinished =Generator.generate_array_minus(metalproofing_plan_30b,metalproofing_finished)
+
+# 前一个月缺数、完工数、核定产能对比
+send_event('mentalproofing_capacity',{date:date_30b,finished:metalproofing_finished,unfinished:metalproofing_unfinished})
+# （一个月）每日完工数占计划数比率
+send_event('mentalproofing_finishrate',{finished:Generator.generate_array_sum(metalproofing_finished),plan:Generator.generate_array_sum(metalproofing_plan_30b)})
+# 前一个月的累计误期值
+send_event('mentalproofing_unfinished',{unfinished:Generator.generate_array_sum(metalproofing_unfinished)})
+# 总累计误期占总计划量的比值
+send_event('mentalprooging_unfinishedrate',{unfinished:Generator.generate_array_sum(metalproofing_unfinished),plan:Generator.generate_array_sum(metalproofing_plan_30b)})
+
+###########
+# 钢塑订单表
+###########
+#---------------------------
+#@@今日订单份数 //定时job
+#@@今日订单总量 //定时job
+#@@当前采购金额 //定时job
+#@@当前收费金额 //定时job
+#---------------------------
+#@@ 前30天订单份数
+order_catenum = Generator.generate_data(20,20,1)
+#@@ 美特斯帮威 羽绒服 前30天订单总量
+order_sumnumA = prand.rand(400..2000)
+#@@ 美特斯帮威 运动服 前30天订单总量
+order_sumnumB = prand.rand(15000..30000)
+#@@ 美特斯帮威 外套 前30天订单总量
+order_sumnumC = prand.rand(30000..40000)
+#@@ 一段时间按订单类型占比分析
+
+#@@ 一段时间内订单数量的同期对比
+order_lastyear_30b = Generator.generate_data(20,250000,0)
+order_thisyear_30b = Generator.generate_data(20,250000,0)
+
+# 一段时间内客户A的订单份数
+send_event('order_cusordercate',{date:date_30b,ordercate:order_catenum})
+
+# 客户A的订单类型的各个数量
+send_event('order_cusordernum',{down:order_sumnumA,sports:order_sumnumB,jacket:order_sumnumC})
+
+# 一段时间按订单类型占比分析
+
+# 一段时间内订单数量的同期对比
+send_event('order_sameperiod',{date:date_30b,last:order_lastyear_30b,current:order_thisyear_30b})
+
+#############
+#销售对比
+#############
+# 一段时间华北采购与收费金额对比
+send_event('sale_hubei',{fee:prand.rand(360000...400000),but:prand.rand(360000...400000)})
+
+# 一段时间内，台州分公司，上海分公司，温州分公司采购金额对比
+send_event('sale_tws_buy',{shanghai:prand.rand(10000..500000),weizhou:prand.rand(20000..90000),taizhou:prand.rand(200000..300000)})
+
+# 一段时间内各分厂的采购金额占比
+send_event('sale_allsub_but',{taizhou:prand.rand(10000..500000),shanghai:prand.rand(10000..500000),wenzhou:prand.rand(10000..500000),dayang:prand.rand(10000..500000),chengdu:prand.rand(10000..500000),guangdong:prand.rand(10000..500000),qingdao:prand.rand(10000..500000),shijiazhuang:prand.rand(10000..500000),xiamen:prand.rand(10000..500000)})
+
+# 一段时间内总客户订单份数的排行汇总
+send_event('sale_order_catenum',{metersbown:prand.rand(10..70),semart:prand.rand(10..70),only:prand.rand(10..70),hm:prand.rand(10..70),zara:prand.rand(10..70)})
+
+# 一段时间内总客户订单金额的排行汇总
+send_event('sale_order_sumnum',{metersbown:prand.rand(5000..500000),semart:prand.rand(5000..500000),only:prand.rand(5000..500000),hm:prand.rand(5000..500000),zara:prand.rand(5000..500000)})
+
+#############
+#OEE&TEEP
+#############
+# 当前平均OEE
+send_event('oeeteep_currentoee',{current:prand.rand(40.0..50.0)})
+
+# 当前平均TEEP
+send_event('oeeteep_currentteep',{current:prand.rand(40.0..42.0)})
+
+# 某生产车间与全厂平均的OEE的对比
+send_event('oeeteep_facoee',{factory:prand.rand(40.0..48.0),all:prand.rand(36.0..48.0)})
+
+# 某生产车间与全厂平均的TEEP的对比
+send_event('oeeteep_facteep',{factory:prand.rand(39.7..48.8),all:prand.rand(32.0..45.0)})
+
+# 各生产车间OEE的前三名排行
+send_event('oeeteep_fst3oee',{first:prand.rand(55.0..58.0),second:prand.rand(48.0..53.0),third:prand.rand(45.0..48.0)})
+
+# 各生产车间TEEP的前三名排行
+send_event('oeeteep_fst3teep',{first:prand.rand(53.0..55.0),second:prand.rand(46.0..48.0),third:prand.rand(43.0..46.0)})
+
+# 各生产车间OEE的后三名排行
+send_event('oeeteep_lst3oee',{first:prand.rand(33.0..35.0),second:prand.rand(36.0..39.0),third:prand.rand(36.0..39.0)})
+
+# 各生产车间TEEP的后三名排行
+send_event('oeeteep_lst3teep',{first:prand.rand(31.0..33.0),second:prand.rand(34.7..45.8),third:prand.rand(36.5..40.0)})
--- modules/generator.rb
+++ modules/generator.rb
@@ -1,13 +1,15 @@
 module Generator
   SECONDS_IN_DAY = 24*60*60
-
   #===========================
   #generate date
   #===========================
-  def self.generate_date interval
+  def self.generate_date interval,end_time=nil
+    if end_time.nil?
+      end_time = Time.now
+    end
     date = []
     for i in 0..(interval.to_i-1)
-      date[i] = (Time.now - (interval.to_i-i-1)*SECONDS_IN_DAY).strftime("%F")
+      date[i] = (end_time - (interval.to_i-i-1)*SECONDS_IN_DAY).strftime("%m.%d")
     end
     date
   end
@@ -20,6 +22,35 @@ module Generator
     prand = Random.new(1234)
     for i in 0..(interval.to_i-1)
       data[i] = prand.rand(min..max)
+    end
+    data
+  end
+
+  #generate_new_array
+  def self.generate_new_array(length,targetlength,oriarray)
+    data = []
+    for i in 0..length-1
+      if i<targetlength
+        data[i] = oriarray[i]
+      end
+      data[i] = 0
+    end
+    data
+  end
+
+  #===========================
+  #generate tricky data
+  #===========================
+  def self.generate_trcky_data(oriarray,interval)
+    data = []
+    prand = Random.new(1234)
+    for i in 0..(oriarray.length-1)
+      if i < interval
+        data[i] = oriarray[i]*prand.rand(0.9..0.95)
+      elsif i == interval
+        data[i] = oriarray[i]*prand.rand(0..1.0)
+      end
+      data[i] = 0
     end
     data
   end
@@ -43,10 +74,38 @@ module Generator
     array.inject{|sum,x| sum + x}
   end
 
+  #===========================
+  #generate array monus
+  #===========================
+  def self.generate_array_minus oriarray,targetarray
+    if oriarray.length != targetarray.length
+      0
+    else
+      data = []
+      for i in 0..oriarray.length-1
+        data[i] = oriarray[i]-targetarray[i]
+      end
+      data
+    end
+  end
+
   #week average
   def self.gererate_week_average array
+    #set today as the end day
+    day_start = 0
+    day_end = 0
+    val = 0
+    count = 0
+    current_date = Time.now
     data = []
+    #for i in 0..array.length-1
+    #  date = current_date - (array.length-i-1)*SECONDS_IN_DAY
+    #  if date.wday < 6
+    #    count=count+1
+    #  else
 
+    #  end
+    #end
     data
   end
 
